a8700d079f086bdcac281d8d3897789dca52b2ca
Maryssun <maryssol.dayanne@gmail.com>
2026-05-28 22:56:32 -0300

    docs: adiciona descrição do projeto e requisitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 function App() {
   return (
     <div>
-      <h1>Corre</h1>
+      <h1> Corre</h1>
       <p>O app da correria.</p>
+
+      <hr />
+
+      <h2>Lançamento do Dia</h2>
+
+      <div>
+        <label>Ganho:</label>
+        <input type="number" />
+      </div>
+
+      <div>
+        <label>Combustível:</label>
+        <input type="number" />
+      </div>
+
+      <div>
+        <label>Alimentação:</label>
+        <input type="number" />
+      </div>
+
+      <div>
+        <label>Manutenção:</label>
+        <input type="number" />
+      </div>
+
+      <div>
+        <label>Outros:</label>
+        <input type="number" />
+      </div>
+
+      <button>Salvar</button>
     </div>
   )
 }
